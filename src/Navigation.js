28f1ad75d20bf9d64b7508ecf24dcc0d46f551bb
Ryotaro Hayashi <92127242+ryo884-2177391@users.noreply.github.com>
2023-07-23 18:27:58 -0700

    Update Navigation.js
    commit

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,7 +8,6 @@ export function NavBar() {
             <span aria-label="Home"><NavLink to="/home"><img src="img/icon.png" alt="logo"></img></NavLink></span>
             <a href="index.html"></a>
             </div>
-
             <div className="nav_right">
                 <NavLink to="/home">Home</NavLink>
                 <NavLink to="/create-post">Create Post</NavLink>
