adc51f1d11e18489ac4f6563e085cdeaed5124ae
Aviraj <avirajdhooria2001@gmail.com>
2023-07-24 18:13:57 -0700

    Added Upload Image Button in CreatePost Component

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Flex,
   useTheme,
@@ -14,10 +14,24 @@ import "@aws-amplify/ui-react/styles.css";
 function CreatePost() {
   const { tokens } = useTheme();
 
+  //Add location
   const locations = ["Kane 220", "MGH 430", "Denny 374"];
   const locationOption = locations.map((location) => {
     return <option value={location}>{location}</option>;
   });
+
+  //Upload Image
+  const [initialImage, updateUploadedImage] = useState("/img/upload_image.png");
+  const fileInput = useRef();
+
+  const handleUploadClick = () => {
+    fileInput.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    updateUploadedImage(URL.createObjectURL(file));
+  };
 
   return (
     <Flex
@@ -41,6 +55,27 @@ function CreatePost() {
           {locationOption}
         </SelectField>
       </Flex>
+      <Card variation="outlined" borderRadius={tokens.radii.large}>
+        <Flex direction={"column"} alignItems={"center"}>
+          <Image src={initialImage} />
+          <input
+            type="file"
+            ref={fileInput}
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+            accept="image/*"
+            multiple
+          />
+          <Button
+            variation="primary"
+            size="large"
+            borderRadius={tokens.radii.large}
+            onClick={handleUploadClick}
+          >
+            Upload now
+          </Button>
+        </Flex>
+      </Card>
     </Flex>
   );
 }
