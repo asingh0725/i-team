b6689f112bca3716738cf4179de7963416694557
Aviraj <avirajdhooria2001@gmail.com>
2023-07-26 13:21:53 -0700

    Added Link to Logo to the Homepage

--- src/Navigation.js
+++ src/Navigation.js
@@ -6,8 +6,10 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <img src="img/header_logo.png" alt="Logo" />
-        <span>SHARE-A-BITE</span>
+        <Link to={"/home"} style={{ textDecoration: "none" }}>
+          <img src="img/header_logo.png" alt="Logo" />
+          <span>SHARE-A-BITE</span>
+        </Link>
       </div>
       <div className="navbar__links">
         <Link to="/home">Home</Link>
