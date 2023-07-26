63db93ec6a95744adeb73ee16dde7dee14195d0f
Ryotaro Hayashi <92127242+ryo884-2177391@users.noreply.github.com>
2023-07-25 18:45:38 -0700

    Update App.js
    Remove { } from import NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { NavBar } from './Navigation';  // assuming Navigation.js is in the same directory
+import NavBar from './Navigation';  // assuming Navigation.js is in the same directory
 import CreatePost from './CreatePost';  // assuming CreatePost.js is in the same directory
 import './App.css';
 
