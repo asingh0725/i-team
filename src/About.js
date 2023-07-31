40b055968b833e758aae0fc9ac3d2aea47370f94
ryo884-2177391 <ryo884@uw.edu>
2023-07-30 22:09:12 -0700

    integrated the amplify ui components to these files

--- src/About.js
+++ src/About.js
@@ -1,25 +1,73 @@
 import React from "react";
-import "./App.css";
+import { Flex, Text } from "@aws-amplify/ui-react";
 
 const About = () => {
   return (
-    <div className="about">
-      <main className="about-content">
-        <h1 className="about-title">About Us</h1>
-        <div className="about-section">
-          <h2 className="about-section-title">Why Share-A-Bite?</h2>
-          <p className="about-section-content">
+    <Flex
+      direction="column"
+      minHeight="100vh"
+      padding="2rem"
+      backgroundColor="#4b2e83"
+      color="white"
+    >
+      <Flex
+        direction="column"
+        alignItems="start"
+        justifyContent="center"
+        flexGrow={1}
+        fontFamily="Encode Sans"
+      >
+        <Text
+          as="h1"
+          textAlign="center"
+          fontSize="2rem"
+          marginBottom="1rem"
+        >
+          About Us
+        </Text>
+
+        <Flex direction="column" marginBottom="2rem">
+          <Text
+            as="h2"
+            textAlign="left"
+            fontSize="2rem"
+            marginBottom="1rem"
+          >
+            Why Share-A-Bite?
+          </Text>
+          <Text
+            as="p"
+            textAlign="left"
+            fontSize="1.2rem"
+            lineHeight="1.6"
+            marginBottom="2rem"
+            width="60%"
+          >
             Share-A-Bite is a platform that enables clubs and organizations to
             share information about their surplus food with hungry students at
             the University of Washington Seattle. By facilitating this exchange,
             Share-A-Bite not only helps address food insecurity among students
             but also contributes to the reduction of food waste.
-          </p>
-        </div>
+          </Text>
+        </Flex>
 
-        <div className="about-section">
-          <h2 className="about-section-title">How do I use it?</h2>
-          <p className="about-section-content">
+        <Flex direction="column" marginBottom="2rem">
+          <Text
+            as="h2"
+            textAlign="left"
+            fontSize="2rem"
+            marginBottom="1rem"
+          >
+            How do I use it?
+          </Text>
+          <Text
+            as="p"
+            textAlign="left"
+            fontSize="1.2rem"
+            lineHeight="1.6"
+            marginBottom="2rem"
+            width="60%"
+          >
             1. Log in using your UW netID <br />
             2. After logging in with your UW netID, you will gain access to
             Share-A-Bite's feed, which is filled with posts about available food
@@ -27,10 +75,10 @@ const About = () => {
             3. To create a post, simply click on the "create post" button and
             provide all the necessary information as prompted <br />
             4. All posts get deleted after 24hrs
-          </p>
-        </div>
-      </main>
-    </div>
+          </Text>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
 
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { Flex, Text } from '@aws-amplify/ui-react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar__logo">
-        <Link to={"/home"} style={{ textDecoration: "none" }}>
-          <img src="img/header_logo.png" alt="Logo" />
-          <span>SHARE-A-BITE</span>
+    <Flex
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      padding="20px"
+      backgroundColor="#c0c0c0"
+    >
+      <Flex alignItems="center">
+        <Link to="/home" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none' }}>
+          <img src="img/header_logo.png" alt="Logo" style={{ height: '50px', marginRight: '10px' }} />
+          <Text color="#fff" fontSize="24px" fontWeight="bold">
+            SHARE-A-BITE
+          </Text>
         </Link>
-      </div>
-      <div className="navbar__links">
-        <Link to="/home">Home</Link>
-        <Link to="/create-post">Create Post</Link>
-        <Link to="/about">About</Link>
-      </div>
-    </nav>
+      </Flex>
+      <Flex direction="row" gap="20px" style={{ fontSize: 'larger' }}>
+        <Link to="/home" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+        <Link to="/create-post" style={{ color: '#fff', textDecoration: 'none' }}>Create Post</Link>
+        <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
+      </Flex>
+    </Flex>
   );
 };
 
