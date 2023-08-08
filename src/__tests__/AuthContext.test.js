eb871e5f26369d9d3beaea35e3c7449dbcc84a75
Aviraj <avirajdhooria2001@gmail.com>
2023-08-07 22:47:33 -0700

    Revert "Started Writing Tests for Components"
    This reverts commit 21460675830dc1afe9eaa2c3f82799179ddd0d2e.

--- src/__tests__/AuthContext.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { AuthProvider } from "../AuthContext";
-
-describe("<AuthProvider />", () => {
-  it("renders without crashing", () => {
-    render(<AuthProvider />);
-  });
-});
